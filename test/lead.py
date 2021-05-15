62d171768e58a08689039a30535544b3429a7b8e
Arunkumar Nagara <Arunkumarcit@gmail.com>
2021-05-15 12:56:05 +0000

    Lead Future tested on QA -prestaging environment

---
+++ test/lead.py
@@ -0,0 +1,13 @@
+# Python code contain all the functionalities
+#create load
+
+def create_load();
+ print "intialtest";
+
+ #update lead
+ def update_lead();
+ print "Updatelead";
+ 
+ #delete lead
+def delete_lead();
+print "delete lead";
